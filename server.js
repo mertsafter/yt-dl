a3f7df3a3e82c7af8a4567f4fd77915026b2f2b8
Mert Safter <mertsafter@online.de>
2023-08-02 20:39:47 +0200

    really important hotfix

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
+var sanitize = require("sanitize-filename");
+var ProgressBar = require('progress');
+
+
 
 const data = fs.readFileSync('./songs.txt', { encoding: 'UTF-8', flag: 'r' })
 
-const lines = data.split(/\r?\n/).map(line => line.trim())
-
+const lines = data.split(/\r?\n/).filter(line => line.length).map(line => line.trim())
+var bar = new ProgressBar(`:bar :current/:total`, { total: lines.length });
 download(lines)
 
 async function download(lines) {
     for (let line of lines) {
+        bar.tick(1)
         const info = await ytdl.getBasicInfo(line)
-        console.log()
         ytdl('https://www.youtube.com/watch?v=aLRletzlJ-4', { quality: 'highestaudio' })
-            .pipe(fs.createWriteStream(`./songs/${info.videoDetails.title}.mp3`));
+            .pipe(fs.createWriteStream(`./songs/${sanitize(info.videoDetails.title)}.mp3`));
     }
 }
 
